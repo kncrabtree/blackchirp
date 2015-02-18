2d57dde209a6555e8c05a610e9b3108e200a247c
Kyle N. Crabtree <kncrabtree@ucdavis.edu>
2015-02-18 15:19:25 -0800

    Added some data members to Experiment class

--- experiment.cpp
+++ experiment.cpp
@@ -3,9 +3,15 @@
 class ExperimentData : public QSharedData
 {
 public:
-    ExperimentData() : number(0) {}
+    ExperimentData() : number(0), isInitialized(false), isAborted(false), isDummy(false) {}
 
     int number;
+    QList<QPair<double,QString> > gasSetpoints;
+    QList<QPair<double,QString> > pressureSetpoints;
+    QDateTime startTime;
+    bool isInitialized;
+    bool isAborted;
+    bool isDummy;
 };
 
 Experiment::Experiment() : data(new ExperimentData)
@@ -33,5 +39,71 @@ Experiment::~Experiment()
 int Experiment::number() const
 {
     return data->number;
+}
+
+QList<QPair<double, QString> > Experiment::gasSetpoints() const
+{
+    return data->gasSetpoints;
+}
+
+QList<QPair<double, QString> > Experiment::pressureSetpoints() const
+{
+    return data->pressureSetpoints;
+}
+
+QDateTime Experiment::startTime() const
+{
+    return data->startTime;
+}
+
+bool Experiment::isInitialized() const
+{
+    return data->isInitialized;
+}
+
+bool Experiment::isAborted() const
+{
+    return data->isAborted;
+}
+
+bool Experiment::isDummy() const
+{
+    return data->isDummy;
+}
+
+void Experiment::setGasSetpoints(QList<QPair<double, QString> > list)
+{
+    data->gasSetpoints = list;
+}
+
+void Experiment::addGasSetpoint(double setPoint, QString name)
+{
+    data->gasSetpoints.append(qMakePair(setPoint,name));
+}
+
+void Experiment::setPressureSetpoints(QList<QPair<double, QString> > list)
+{
+    data->pressureSetpoints = list;
+}
+
+void Experiment::addPressureSetpoint(double setPoint, QString name)
+{
+    data->pressureSetpoints.append(qMakePair(setPoint,name));
+}
+
+void Experiment::setInitialized()
+{
+    data->isInitialized = true;
+    data->startTime = QDateTime::currentDateTime();
+}
+
+void Experiment::setAborted()
+{
+    data->isAborted = true;
+}
+
+void Experiment::setDummy()
+{
+    data->isDummy = true;
 }
 
--- experiment.h
+++ experiment.h
@@ -2,6 +2,8 @@
 #define EXPERIMENT_H
 
 #include <QSharedDataPointer>
+#include <QPair>
+#include <QDateTime>
 
 class ExperimentData;
 
@@ -14,6 +16,20 @@ public:
     ~Experiment();
 
     int number() const;
+    QList<QPair<double,QString> > gasSetpoints() const;
+    QList<QPair<double,QString> > pressureSetpoints() const;
+    QDateTime startTime() const;
+    bool isInitialized() const;
+    bool isAborted() const;
+    bool isDummy() const;
+
+    void setGasSetpoints(QList<QPair<double,QString> > list);
+    void addGasSetpoint(double setPoint, QString name);
+    void setPressureSetpoints(QList<QPair<double,QString> > list);
+    void addPressureSetpoint(double setPoint, QString name);
+    void setInitialized();
+    void setAborted();
+    void setDummy();
 
 private:
     QSharedDataPointer<ExperimentData> data;
